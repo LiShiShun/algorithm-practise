449ec03a0700104b2fb1e400d07b040b12bfcbfc
lipeiyuan <myselflpy@gmail.com>
2025-03-17 22:24:59 +0800

    增加最长回文子串算法

---
+++ src/palindrome/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+std::string GetMaxPalindromeSubstring(const char* s) {
+    if(s == nullptr || s[0] == '\0') {
+        return "";
+    }
+
+    return "";
+}
+
+int main() {
+    return 0;
+}
+
+
+
