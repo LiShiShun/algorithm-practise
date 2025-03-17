7ffdb7b228e7911383c7fcd821e4abca6b933a0b
lipeiyuan <myselflpy@gmail.com>
2025-03-17 22:37:50 +0800

    临时提交回文字符串O(n^2)实现

--- src/palindrome/main.cpp
+++ src/palindrome/main.cpp
@@ -2,13 +2,31 @@
 #include <string>
 #include <cassert>
 
-std::string GetMaxPalindromeSubstring(const char* s) {
+int ExpandToBoundary(const char* s, int left, int right) {
+    int from = left;
+    int to = right;
+    while(from >= 0 && s[to] != '\0' && s[from] == s[to]) {
+        from--;
+        to++;
+    }
+
+    return to - from - 1;
+}
+
+// O(n^2)
+std::string GetMaxPalindromeSubstring(const char* s, int len) {
     if(s == nullptr || s[0] == '\0') {
         return "";
     }
 
+    for(int i = 0;s[i] != '\n'; i++) {
+        
+    }
+
     return "";
 }
+
+// O(n)
 
 int main() {
     return 0;
